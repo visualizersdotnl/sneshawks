17be4f8e9ff1058f0d43193982a5dd8115299d9c
Niels J. de Wit <ndewit@gmail.com>
2015-09-22 00:18:01 +0200

    not sure what to say

--- main.asm
+++ main.asm
@@ -1,7 +1,11 @@
 
 ; to do:
-; - constants for important (address) values (system.inc)
-; - everything
+; - steal bits of knowledge from nevitski's kit
+;   + memory/register constants
+;   + figure out fast ROM?
+;   + on boot initialization of registers & memory
+; - figure out if it's going to be metaprogramming or a C compiler
+; - draw a f*cking sprite already!
 
 .INCLUDE "header.inc"
 .INCLUDE "system.inc"
@@ -17,11 +21,11 @@ Main:
 	lda #$1f
 	sta cycle
 
-	; set mode 7 w/small tiles
+	; set mode 7 w/small tiles (FIXME)
 	lda #$7
 	sta $2105
 
-	; fire up display & VBLANK
+	; fire up display & NMI
 	jsr StartDisplay
 
 @infinity:
@@ -29,6 +33,13 @@ Main:
 	jmp @infinity
 
 Int_NMI:
+	_XY16_A16
+	pha
+	phx
+	phy
+
+	_XY16_A8
+
 	; cycle
 	lda cycle
 	ina
@@ -43,9 +54,14 @@ Int_NMI:
 	; we're done, clear NMI flag
 	lda $4210
 
+	_XY16_A16
+	ply
+	plx
+	pla
+
 	rti
 
-; first byte in lower system RAM
+; first byte in bank
 .EQU cycle $0000
 
 .ENDS
